367f80605f6e2d218451bd103b8f9e69f0b2c2bb
Jason Cho <snapbuy@naver.com>
2025-05-29 10:47:06 +0900

    new file:   test_antropic_api.py

---
+++ test_antropic_api.py
@@ -0,0 +1,22 @@
+import anthropic
+
+client = anthropic.Anthropic()
+
+message = client.messages.create(
+    model="claude-opus-4-20250514",
+    max_tokens=1000,
+    temperature=1,
+    system="You are a world-class poet. Respond only with short poems.",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Why is the ocean salty?"
+                }
+            ]
+        }
+    ]
+)
+print(message.content)
